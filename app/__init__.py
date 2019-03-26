dfc3cae6920566ef60348a445a82a4f048195529
Stephanie Jones <stephanie.jones@mymona.uwi.edu>
2019-03-26 22:22:12 +0000

    fixed error

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from newsapi import NewsApiClient
 
-newsapi = NewsApiClient(api_key='ebaf80aa3ba04c4ab977be6d14771cd2')
+newsapi = NewsApiClient(api_key='')
 
 app = Flask(__name__)
 from app import views
